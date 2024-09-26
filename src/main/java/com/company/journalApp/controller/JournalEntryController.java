c3461446084acc09e31b2ac6a2f29bdcf78dded7
Ankit <tomarankit78@gmail.com>
2024-09-26 22:45:48 +0530

    First commit of Journal App

---
+++ src/main/java/com/company/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,43 @@
+package com.company.journalApp.controller;
+
+import com.company.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+
+    private Map<Long, JournalEntry> JournalEntries =new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll(){   //localhost:8080/journal GET
+        return new ArrayList<>(JournalEntries.values());
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){   // //localhost:8080/journal POST
+        JournalEntries.put(myEntry.getId(), myEntry);
+        return true;
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getEntryById(@PathVariable Long myId){
+         return JournalEntries.get(myId);
+    }
+
+    @DeleteMapping ("id/{myId}")
+    public JournalEntry deleteEntryById(@PathVariable Long myId){
+        return JournalEntries.remove(myId);
+    }
+    @PutMapping("id/{id}")
+    public JournalEntry updateEntryById(@PathVariable Long id,@RequestBody JournalEntry myEntry){
+        return JournalEntries.put(id, myEntry);
+    }
+
+
+}
